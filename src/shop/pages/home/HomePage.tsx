44ad3fef683bb022b1c5bf61b8b5580e040bdabe
Angel Guzman <angelteo0510@gmail.com>
2026-04-11 20:33:47 -0600

    HomePage - Paginación y productos

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,7 +1,14 @@
+import { CustomPagination } from "../../../components/custom/CustomPagination"
+import { products } from "../../../mocks/products.mock"
 import { CustomJumbotron } from "../../components/CustomJumbotron"
+import { ProductsGrid } from "../../components/ProductsGrid"
 
 export const HomePage = () => {
   return (
-    <CustomJumbotron title="Todos los productos"/>
+    <>
+      <CustomJumbotron title="Todos los productos" />
+      <ProductsGrid products={products}/>
+      <CustomPagination totalPages={5} />
+    </>
   )
 }
