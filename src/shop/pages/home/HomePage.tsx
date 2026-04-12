46c5a93333d4d373b603977d22ecfc94da9a5919
Angel Guzman <angelteo0510@gmail.com>
2026-04-11 19:15:54 -0600

    Instalación de Tailwind, ShadCN UI y Google Fonts

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Button } from '../../../components/ui/button'
 
 export const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>HomePage
+      <h1>Hola, Mundo</h1>
+      <h1 className='font-monserrat'>Hola, Mundo</h1>
+      <h1 className='font-monserrat font-thin'>Hola, Mundo</h1>
+      <h1 className='font-monserrat font-normal'>Hola, Mundo</h1>
+      <h1 className='font-monserrat font-bold'>Hola, Mundo</h1>
+      <Button>Hola, mundo</Button>
+    </div>
   )
 }
