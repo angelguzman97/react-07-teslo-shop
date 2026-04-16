3c042dfedee9191d4c02a8f74175a0b9a49d1c5e
Angel Guzman <angelteo0510@gmail.com>
2026-04-15 19:09:17 -0600

    Lógica adicional de la búsqueda

--- src/shop/components/CustomHeader.tsx
+++ src/shop/components/CustomHeader.tsx
@@ -1,10 +1,30 @@
 import { Search, ShoppingBag, Menu } from "lucide-react";
 import { Button } from "../../components/ui/button";
 import { Input } from "../../components/ui/input";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { useSearchParams } from "react-router";
 
 export const CustomHeader = () => {
     const [cartCount, setCartCount] = useState(3);
+
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const inputRef = useRef<HTMLInputElement>(null);
+    const query = searchParams.get('query') || '';
+
+    const handleSearch = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key !== 'Enter') return;
+        const query = inputRef.current?.value;
+        const newSearchParams = new URLSearchParams(); // Agregar nueva url
+        if (!query) {
+            newSearchParams.delete('query');
+        } else {
+            newSearchParams.set('query', inputRef.current!.value); // Agregar el parametro
+        };
+
+        setSearchParams(newSearchParams); // Esto limpia la url al realizar una busqueda
+    }
+
     return (
         <header className="sticky top-0 z-50 w-full border-b backdrop-blur bg-slate-50">
             <div className="container mx-auto px-4 lg:px-8">
@@ -38,7 +58,12 @@ export const CustomHeader = () => {
                         <div className="hidden md:flex items-center space-x-2">
                             <div className="relative">
                                 <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
-                                <Input placeholder="Buscar productos..." className="pl-9 w-64 h-9" />
+                                <Input
+                                    ref={inputRef}
+                                    placeholder="Buscar productos..."
+                                    className="pl-9 w-64 h-9 bg-white"
+                                    onKeyDown={handleSearch}
+                                    defaultValue={query} />
                             </div>
                         </div>
 
