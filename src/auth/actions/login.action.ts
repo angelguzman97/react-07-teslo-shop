133c485dbe03a83fd19accfc437771a0aac73161
Angel Guzman <angelteo0510@gmail.com>
2026-04-22 23:29:32 -0600

    Mensajes en pantalla sobre inicio de sesión

--- src/auth/actions/login.action.ts
+++ src/auth/actions/login.action.ts
@@ -1,12 +1,12 @@
 import { tesloApi } from "../../api/tesloApi"
 import type { AuthResponse } from "../interfaces/auth.response"
 
-export const loginAction = async (username: string, password: string): Promise<AuthResponse> => {
+export const loginAction = async (email: string, password: string): Promise<AuthResponse> => {
     const URL_BASE = import.meta.env.VITE_API_URL;
     // Puede haber errores conexion, credenciales, etc.
     try {
         const { data } = await tesloApi.post<AuthResponse>(`${URL_BASE}/auth/login`, {
-            username,
+            email,
             password
         });
 
--- src/auth/pages/login/LoginPage.tsx
+++ src/auth/pages/login/LoginPage.tsx
@@ -1,22 +1,40 @@
-import { Input } from "@base-ui/react";
+import { Link, useNavigate } from "react-router";
+import { useState, type FormEvent } from "react";
 import { Card, CardContent } from "../../../components/ui/card";
 import { Label } from "../../../components/ui/label";
 import { Button } from "../../../components/ui/button";
 import { CustomLogo } from "../../../components/custom/CustomLogo";
-import { Link } from "react-router";
-import type { FormEvent } from "react";
+import { loginAction } from "../../actions/login.action";
+import { Input } from "../../../components/ui/input";
+import { toast } from "sonner";
 
 export const LoginPage = () => {
+
+  const navigate = useNavigate();
+  const [isPosting, setIsPosting] = useState(false);
 
   // Crear manejador para formulario
   const handleLogin = async (event: FormEvent<HTMLFormElement>) => {
     // Al mandar el preventDefault se evita el render en la pag.
     event.preventDefault();
+    setIsPosting(true);
     const formData = new FormData(event.target as HTMLFormElement);
     const email = formData.get('email') as string;
-    const password = formData.get('password');
+    const password = formData.get('password') as string;
 
-    console.log({ email, password });
+    try {
+      const data = await loginAction(email, password);
+      // Si sale bien se guarda el token en el localStorage
+      localStorage.setItem('token', data.token);
+
+      console.log('Re-redireccionando al home');
+      navigate('/');
+
+    } catch (error) {
+      toast.error('Correo o/y contraseña no válidos');
+    }
+
+    setIsPosting(false);
 
   }
   return (
@@ -42,7 +60,7 @@ export const LoginPage = () => {
                 </div>
                 <Input id="password" name="password" type="password" required placeholder="Contraseña" />
               </div>
-              <Button type="submit" className="w-full">
+              <Button type="submit" className="w-full" disabled={isPosting}>
                 Ingresar
               </Button>
               <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
