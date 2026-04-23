994106d1dcbe280f826a49219dcc19e740dceace
Angel Guzman <angelteo0510@gmail.com>
2026-04-22 23:53:18 -0600

    Gestor de estado - Zustand

--- src/shop/pages/product/ProductPage.tsx
+++ src/shop/pages/product/ProductPage.tsx
@@ -1,5 +1,14 @@
+import { useCounterStore } from "../../../auth/store/auth.store"
+import { Button } from "../../../components/ui/button"
+
 export const ProductPage = () => {
+  const { count, inc, dec, incBy } = useCounterStore()
   return (
-    <div>PagePage</div>
+    <>
+      <h1 className="text-3xl font-monserrat">{count}</h1>
+      <Button onClick={inc}>+1</Button>
+      <Button onClick={dec}>-1</Button>
+      <Button onClick={() => incBy(2)}>+2</Button>
+    </>
   )
 }
