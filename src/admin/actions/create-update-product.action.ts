af62b01e37e08da6cc3a803a85cd2a5dedc41269
Angel Guzman <angelteo0510@gmail.com>
2026-04-29 17:08:49 -0600

    Acción - Actualizar o crear producto

---
+++ src/admin/actions/create-update-product.action.ts
@@ -0,0 +1,30 @@
+import { tesloApi } from "../../api/tesloApi";
+import type { Product } from "../../interfaces/product.interface";
+
+export const createUpdateProductAction = async (productLike: Partial<Product>): Promise<Product> => {
+
+    const { id, user, images = [], ...rest } = productLike;
+
+    const isCreating = id === 'new';
+
+    // Transformar a number
+    rest.stock = Number(rest.stock || 0);
+    rest.price = Number(rest.price || 0);
+
+    // Se puede usar .patch o .post o solo mandar el objeto
+    const { data } = await tesloApi<Product>({
+        url: isCreating ? '/products' : `/products/${id}`,
+        method: isCreating ? 'POST' : 'PATCH',
+        data: rest
+
+    });
+
+    return {
+        ...data,
+        images: data.images.map((image) => {
+            if (image.includes('http')) return image;
+            return `${import.meta.env.VITE_API_URL}/files/product/${image}`;
+        })
+    };
+
+}
