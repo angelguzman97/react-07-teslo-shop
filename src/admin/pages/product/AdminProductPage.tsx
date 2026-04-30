54d17ecded0bc3a90d5c0d2916b113a8cd9b29ea
Angel Guzman <angelteo0510@gmail.com>
2026-04-30 11:12:21 -0600

    Bloquear botón de submit mientras se postea

--- src/admin/pages/product/AdminProductPage.tsx
+++ src/admin/pages/product/AdminProductPage.tsx
@@ -61,6 +61,7 @@ export const AdminProductPage = () => {
     subtitle={subtitle}
     product={product}
     onSubmit={handleSubmit}
+    isPending={productMutation.isPending}
   />
 
 };
--- src/admin/pages/product/ui/ProductForm.tsx
+++ src/admin/pages/product/ui/ProductForm.tsx
@@ -11,6 +11,7 @@ interface Props {
     title: string;
     subtitle: string;
     product: Product;
+    isPending: boolean;
 
     // Methods
     onSubmit: (productLike: Partial<Product>) => Promise<void>;
@@ -18,7 +19,7 @@ interface Props {
 
 const availableSizes: Size[] = ['XS', 'S', 'M', 'L', 'XL', 'XXL'];
 
-export const ProductForm = ({ title, subtitle, product, onSubmit }: Props) => {
+export const ProductForm = ({ title, subtitle, product, isPending = false, onSubmit }: Props) => {
 
     const [dragActive, setDragActive] = useState(false);
     const { register,
@@ -90,14 +91,14 @@ export const ProductForm = ({ title, subtitle, product, onSubmit }: Props) => {
             <div className="flex justify-between items-center">
                 <AdminTitle title={title} subtitle={subtitle} />
                 <div className="flex justify-end mb-10 gap-4">
-                    <Button variant="outline">
+                    <Button type='button' variant="outline">
                         <Link to="/admin/products" className="flex items-center gap-2">
                             <X className="w-4 h-4" />
                             Cancelar
                         </Link>
                     </Button>
 
-                    <Button type="submit">
+                    <Button type="submit" disabled={isPending}>
                         <SaveAll className="w-4 h-4" />
                         Guardar cambios
                     </Button>
